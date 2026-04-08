da22bf3d1ca7ad9c31b1ab1b64a3b6776b5c6159
Mazen Bahgat <mazenbahgat@outlook.com>
2026-04-08 08:43:44 +0200

    Fix Docker Hub login step name and update compose down command in deploy workflow; add 'Tea' task to init.js

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -5,5 +5,6 @@ db.tasks.insertMany([
   { title: 'Eggs', completed: true },
   { title: 'Bread', completed: false },
   { title: 'Butter', completed: false },
-  { title: 'Orange juice', completed: false }
+  { title: 'Orange juice', completed: false },
+  { id: 5, name: 'Tea',  status: 'pending' }
 ]);
